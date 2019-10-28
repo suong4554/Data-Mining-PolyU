6c50ab57d816b0398424de3b83312f577dfdcb9f
DESKTOP-Q9VORRS\Simon Roschmann <sim.ro@freenet.de>
2019-10-28 10:18:53 +0800

    Code and plots of data preprocessing

---
+++ scripts/dataPreprocessing.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('train.csv')
+
+#attributes
+print(df_train.columns)
+
+#saleprice count, min, max, mean, ...
+print(df_train['SalePrice'].describe())
+
+#saleprice skewness and kurtosis
+print("Skewness: %f" % df_train['SalePrice'].skew())
+print("Kurtosis: %f" % df_train['SalePrice'].kurt())
+
+#saleprice histogram
+sns.distplot(df_train['SalePrice']);
+plt.show()
+
+#scatter plot grlivarea/saleprice, above grade (ground) living area square feet
+data = pd.concat([df_train['SalePrice'], df_train['GrLivArea']], axis=1)
+data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0,800000));
+plt.show()
+
+#scatter plot totalbsmtsf/saleprice, total square feet of basement area
+data = pd.concat([df_train['SalePrice'], df_train['TotalBsmtSF']], axis=1)
+data.plot.scatter(x='TotalBsmtSF', y='SalePrice', ylim=(0,800000));
+plt.show()
+
+#box plot overallqual/saleprice, rates the overall material and finish of the house
+data = pd.concat([df_train['SalePrice'], df_train['OverallQual']], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x='OverallQual', y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.show()
+
+#box plot yearbuilt/saleprice, original construction date
+data = pd.concat([df_train['SalePrice'], df_train['YearBuilt']], axis=1)
+f, ax = plt.subplots(figsize=(16, 8))
+fig = sns.boxplot(x='YearBuilt', y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.xticks(rotation=90);
+plt.show()
+
+#correlation matrix
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+plt.show()
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+#scatterplots to compare attributes
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show();
+
+#dealing with missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index,1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+df_train.isnull().sum().max()
+
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('outer range (high) of the distribution:')
+print(high_range)
+
+#bivariate analysis saleprice/grlivarea
+data = pd.concat([df_train['SalePrice'], df_train['GrLivArea']], axis=1)
+data.plot.scatter(x='GrLivArea', y='SalePrice', ylim=(0,800000));
+plt.show()
+
+#deleting noise points
+df_train.sort_values(by = 'GrLivArea', ascending = False)[:2]
+df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
+
+#bivariate analysis saleprice/totalbsmtsf
+data = pd.concat([df_train['SalePrice'], df_train['TotalBsmtSF']], axis=1)
+data.plot.scatter(x='TotalBsmtSF', y='SalePrice', ylim=(0,800000));
+plt.show()
+
+#histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+plt.show()
+
+#applying log transformation to saleprice
+df_train['SalePrice'] = np.log(df_train['SalePrice'])
+#transformed histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+plt.show()
+
+#applying log transformation to grlivarea
+df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
+#transformed histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+plt.show()
+
+#histogram and normal probability plot
+sns.distplot(df_train['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['TotalBsmtSF'], plot=plt)
+plt.show()
+
+#create column for new variable
+#if area>0 set HasBsmt to 1, for area==0 set HasBsmt to 0
+df_train['HasBsmt'] = pd.Series(len(df_train['TotalBsmtSF']), index=df_train.index)
+df_train['HasBsmt'] = 0
+df_train.loc[df_train['TotalBsmtSF']>0,'HasBsmt'] = 1
+
+#transform data
+df_train.loc[df_train['HasBsmt']==1,'TotalBsmtSF'] = np.log(df_train['TotalBsmtSF'])
+
+#histogram and normal probability plot
+sns.distplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], plot=plt)
+plt.show()
+
+#scatter plot
+plt.scatter(df_train['GrLivArea'], df_train['SalePrice']);
+plt.show()
+#scatter plot
+plt.scatter(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], df_train[df_train['TotalBsmtSF']>0]['SalePrice']);
+plt.show()
+
+#convert categorical variable into dummy
+df_train = pd.get_dummies(df_train)
